0b3d89e4d82b461ceb789d20557c3253bde794b0
seedyoon <seedyoon@gmail.com>
2019-07-04 10:53:56 +0900

    vue form 마크업

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <div>
-    <app-header v-on:renew="renewEvent" v-bind:propsdata="str">app</app-header>
-  </div>
+  
 </template>
 
-
-
 <script>
-import AppHeader from "./components/AppHeader.vue";
-
 export default {
-  data: function() {
-    return {
-      str: "my header"
-    };
-  },
-  components: {
-    "app-header": AppHeader
-  },
-  methods: {
-    renewEvent: function() {
-      this.str = "change string";
-    }
-  }
-};
+
+}
 </script>
 
 <style>
+
 </style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <form action>
+    <div>
+      <label for="username">ID :</label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">PW:</label>
+      <input type="password" id="password" />
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
