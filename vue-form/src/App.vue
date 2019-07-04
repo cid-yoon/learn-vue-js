90a74a244e2606f4fd6159666a5fd2d7c3427e66
seedyoon <seedyoon@gmail.com>
2019-07-04 11:12:42 +0900

    axios 통신

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,19 +1,49 @@
 <template>
-  <form action>
+  <form v-on:submit.prevent="submitForm">
     <div>
-      <label for="username">ID :</label>
-      <input id="username" type="text" />
+      <label for="username">ID:</label>
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">PW:</label>
-      <input type="password" id="password" />
+      <input id="password" type="password" v-model="password" />
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    submitForm: function() {
+      // eslint-disable-next-line no-console
+      console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+      axios
+        .post(url, data)
+        .then(function(response) {
+          // eslint-disable-next-line no-console
+          console.log(response);
+        })
+        .catch(function(error) {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        });
+    }
+  }
+};
 </script>
 
 <style>
